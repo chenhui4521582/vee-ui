a794cf11e77629b8e8d50b32d12ff25d72460a16
chenhuihui <461822621@qq.com>
2020-10-30 11:08:21 +0800

    111

--- .history/src/main_20190408184403.js
+++ .history/src/main_20190408184403.js
@@ -4,11 +4,12 @@ import router from './config/router/router'
 import store from './config/store'
 import Vee from './components/'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-Vue.use(Vee)
+Vue.use(Vee);
+
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
